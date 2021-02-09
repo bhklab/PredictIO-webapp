58331d113735fbf92d94453220f13c79bb2f3ac1
Minoru Nakano <minoru.nakano@gmail.com>
2021-02-08 19:40:37 -0500

    added API endpoint to query signature, model and outcome options from the database. Implemented the dropdown logic to select multiple signatures

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 ReactDOM.render(
   <React.Fragment>
