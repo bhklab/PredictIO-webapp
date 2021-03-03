8128680f0cac8c4c0d33001798d98e631d2088b4
Minoru Nakano <minoru.nakano@gmail.com>
2021-03-03 16:57:57 -0500

    [in progress] iTNT data visualization script integration: integrated GSEA and geneset forest plot scripts.

--- resources/itnt_visualization.py
+++ resources/itnt_visualization.py
@@ -16,18 +16,34 @@ class ITNTVisualization(Resource):
         
         try:
             cwd = os.path.abspath(os.getcwd())
-            r_path = os.path.join(cwd, 'r-scripts', 'test1.R')
+            #r_path = os.path.join(cwd, 'r-scripts', 'test1.R')
+            r_path = os.path.join(cwd, 'r-scripts', 'itnt', 'iTNT-all.R')
             p = subprocess.Popen(['Rscript', r_path, 'hello', 'test', 'TRUE', 'FALSE'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            (output, err) = p.communicate()
+            # (output, err) = p.communicate()
+            
+            while True:
+                line = p.stdout.readline()
+                # err = p.stderr.readline()
 
-            error = err.decode("utf-8")
-            if(len(error) > 0):
-                print(error)
+                # if(err):
+                #     print('error')
+                #     print(err.decode("utf-8"))
+                #     break
 
-                res['error'] = 1
-                res['errorMessage'] = error
-            else:
-                res['data'] = json.loads(output)
+                if not line:
+                    break
+
+                print(line.rstrip().decode("utf-8"))
+
+            # error = err.decode("utf-8")
+            # if(len(error) > 0):
+            #     print(error)
+
+            #     # res['error'] = 1
+            #     # res['errorMessage'] = error
+            # else:
+            #     print(output.decode("utf-8"))
+            #     # res['data'] = json.loads(output)
 
         except:
             e = sys.exc_info()[0]
