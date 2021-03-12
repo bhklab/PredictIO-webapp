7acebb9f9a1ca82f87ade22c89da02b4969b0f28
Minoru Nakano <minoru.nakano@gmail.com>
2021-03-12 11:59:15 -0500

    [in progress] API endpoint for on-the-fly analysis

--- resources/async_process.py
+++ resources/async_process.py
@@ -16,6 +16,15 @@ class AsyncProcess(Resource):
             "error": 0,
             "data": []
         }
+
+        analysis_id = '12345678'
+        study = 'Braun,Damotte,Fumet.1,Fumet.2,Hugo,Hwang,Jerby_Arnon,Jung,Liu,Mariathasan,Miao.1,Miao.2,Nathanson,Riaz,Rizvi.15,Rizvi.18,Roh,Samstein,Snyder,Van_Allen'
+        sex = 'M,F'
+        primary	= 'Melanoma,Lung,Kidney'
+        drug_type = 'PD-1/PD-L1,CTLA4'
+        data_type = 'EXP'
+        sequencing_type = 'FPKM,TPM'
+        gene = 'B2M,CD8A,GZMA'
         
         try:
             cwd = os.path.abspath(os.getcwd())
@@ -23,7 +32,7 @@ class AsyncProcess(Resource):
             r_path = os.path.join(cwd, 'r-scripts', 'io_meta', 'Run_Compute_Result.R')
             r_wd = os.path.join(cwd, 'r-scripts', 'io_meta')
 
-            cmd = ['Rscript', r_path, r_wd]
+            cmd = ['Rscript', r_path, r_wd, analysis_id, study, sex, primary, drug_type, data_type, sequencing_type, gene]
 
             def run_in_thread():
                 out = None
@@ -57,12 +66,63 @@ class AsyncProcess(Resource):
         return res, status
 
     def post(self):
-        print("Submitting the IO Predict request.")
+        status = 200
+        res = {
+            "error": 0,
+            "data": []
+        }
 
         # parse request
+        query = request.get_json()
+        
+        try:
+            cwd = os.path.abspath(os.getcwd())
 
-        # call the pipeline with the requenst
+            r_path = os.path.join(cwd, 'r-scripts', 'io_meta', 'Run_Compute_Result.R')
+            r_wd = os.path.join(cwd, 'r-scripts', 'io_meta')
 
-        # return the output data when pipeline is complete
-        return "success", 200
+            cmd = [
+                'Rscript', 
+                r_path, 
+                r_wd, 
+                query['analysisID'], 
+                query['study'], 
+                query['sex'], 
+                query['primary'], 
+                query['drugType'], 
+                query['dataType'], 
+                query['sequencingType'], 
+                query['gene']
+            ]
+
+            def run_in_thread():
+                out = None
+
+                p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                
+                while True:
+
+                    line = p.stdout.readline()
+
+                    if not line:
+                        break
+                    else:
+                        out = line.rstrip().decode("utf-8")
+
+                print(out)
+
+            thread = threading.Thread(target=run_in_thread)
+            thread.start()
+
+            print('thread started')
+
+        except:
+            e = sys.exc_info()[0]
+            print(e)
+
+            res['error'] = 1
+            res['errorMessage'] = e
+            status = 500
+
+        return res, status
     
