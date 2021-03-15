8a468a4dadd6de830623493a296b59bfdf7c755d
deventorum <deventorum@gmail.com>
2021-03-15 15:03:57 -0400

    Added pylint config file

--- db/models/main.py
+++ db/models/main.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from numpy import genfromtxt
 
 # used to get values from .env file
@@ -28,20 +28,16 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 try:
-    # sample CSV file used:  http://www.google.com/finance/historical?q=NYSE%3AT&ei=W4ikVam8LYWjmAGjhoHACw&output=csv
     dir_path = os.path.dirname(os.path.realpath(__file__))
     dataset_file = os.path.join(dir_path, "../seedfiles/dataset.csv")
-    # data = Load_Data(dataset_file, [
-    #     ('dataset_id', int), ('dataset_name', '>U12')])
     dataset_data = Load_Data(dataset_file, [int, '>U12'])
-    for i in data:
+    for i in dataset_data:
         record = dataset_data.Dataset(**{
             'dataset_id': i[0],
             'dataset_name': i[1],
         })
         session.add(record)  # Add all the records
         print('Adding Dataset record')
-
     session.commit()  # Attempt to commit all the records
 except:
     session.rollback()  # Rollback the changes on error
