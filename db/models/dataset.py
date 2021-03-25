b80c11f48c082c939ec8778c20ef53c6e8ec8bad
deventorum <deventorum@gmail.com>
2021-03-25 12:57:25 -0400

    Updated dataset model to hold metadata

--- db/models/dataset.py
+++ db/models/dataset.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Text
 from sqlalchemy.orm import relationship
 from .base import Base
 
@@ -7,5 +7,11 @@ class Dataset(Base):
     __tablename__ = "dataset"
     dataset_id = Column(Integer, primary_key=True, index=True, unique=True)
     dataset_name = Column(String(32))
+    # metadata
+    pmid = Column(Text)
+    title = Column(Text)
+    summary = Column(Text)
+    authors = Column(Text)
+    # relationships to other tables
     clinical_infos = relationship("Patient", backref="dataset")
     dataset_genes = relationship("DatasetGene", backref="dataset")
