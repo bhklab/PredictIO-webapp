653b9839323a8a7a01d998535b9435d7dc784e1c
Minoru Nakano <minoru.nakano@gmail.com>
2021-04-13 11:11:41 -0400

    minor UI position fix

--- client/src/Components/UtilComponents/Layout.js
+++ client/src/Components/UtilComponents/Layout.js
@@ -5,7 +5,7 @@ import Footer from '../Footer/Footer';
 import styled from 'styled-components';
 
 const Main = styled.main`
-    height: 100vh;
+    height: (100vh + 45px);
     width: 85%;
     max-width: 1500px;
     margin-left: auto;
