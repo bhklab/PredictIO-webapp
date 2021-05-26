056805d0f9f71d699ee2db9d725135e2cb1ea94c
Minoru Nakano <minoru.nakano@gmail.com>
2021-05-26 18:48:10 -0400

    resolved the query issue

--- resources/search_gene.py
+++ resources/search_gene.py
@@ -1,18 +1,33 @@
 from flask import request
 from flask_restful import Resource
 from db.models.gene import Gene
+from db.models.patient import Patient
+from db.models.dataset_gene import DatasetGene
 
 class SearchGene(Resource):
-    def get(self):
-        
+    def get(self, datatype):
+        print(datatype)
         # parse request
         query = request.args.get('query')
 
-        # query the genes table
-        genes = Gene.query.filter(Gene.gene_name.ilike('{0}%'.format(query)))
-        result = Gene.serialize_list(genes.all())
+        filtered = DatasetGene.query.group_by(Gene.gene_id)\
+            .join(Gene, DatasetGene.gene_id == Gene.gene_id)\
+            .join(Patient, DatasetGene.dataset_id == Patient.dataset_id)\
+            .add_columns(Gene.gene_id, Gene.gene_name, Patient.expression, Patient.cna, Patient.snv)\
+            .filter(Gene.gene_name.ilike('{0}%'.format(query)))\
+            .order_by(Gene.gene_name)\
+            .all()
+        
+        if(datatype == 'expression'):
+            filtered = [item for item in filtered if item.expression == 1]
+        elif(datatype == 'cna'):
+            filtered = [item for item in filtered if item.cna == 1]
+        elif(datatype == 'snv'):
+            filtered = [item for item in filtered if item.snv == 1]
+        
+        genes = list(map((lambda item: {'gene_id': item.gene_id, 'gene_name': item.gene_name}), filtered))
 
-        return result, 200
+        return genes, 200
     
     def post(self): 
 
